a6879bffb93e08837af4d2d6a53418ee7be12489
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-10 18:58:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,13 +20,12 @@ class Transfer
     if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    
-    elsif  @status == "complete"
-      puts "Transaction was already executed."
-      
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
     else
-      @sender.deposit(@amount * -1)
-      @receiver.deposit(@amount)
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
       @status = "complete"
     end
   end
