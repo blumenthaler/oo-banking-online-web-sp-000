4bda1e6bce44225071ca698b5b0bf9257e04e2f0
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-10 19:09:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction
-    if  @sender.balance < @amount || @sender.status = "closed"
+    if  @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
