686e7f491a52816e00737b19cbe14a97427bab3d
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-10 18:55:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,5 +25,6 @@ class Transfer
       puts "Transaction was already executed."
       
     else
-      @sender.deposit = 
+      @sender.deposit(@amount * -1)
+      @receiver.deposit(@amount)
 end
