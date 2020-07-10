204119d8b2579967b89e873694ad135509bff4ba
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-10 19:12:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,6 +21,8 @@ class Transfer
       @sender.deposit( @amount * -1 ) 
       @receiver.deposit( @amount )
       @status = "complete"
+    elsif self.status == "complete"
+      
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
